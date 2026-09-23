c6da9111612c0e714f451fd85e45fe4154081c66
Georgii Oblapenko <georgii.oblapenko@rwth-aachen.de>
2026-09-23 17:26:39 +0200

    dependencies and stuff

---
+++ src/specqkdynamic2026.jl
@@ -0,0 +1,7 @@
+module specqkdynamic2026
+
+# Placeholder module. This is a reproducibility repository: run the
+# simulations directly, e.g.
+#     julia --project=. simulations/<simname>.jl
+
+end
